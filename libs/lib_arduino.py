b6a43ea9a94bc837df7899585097ccf6eaab184f
Florent HUGOUVIEUX <florent.hugouvieux@etu.umontpellier.fr>
2020-01-11 12:39:42 +0100

    Remove dead code

--- libs/lib_arduino.py
+++ libs/lib_arduino.py
@@ -10,23 +10,6 @@ serialArduino = serial.Serial('/dev/ttyACM0',timeout=1, baudrate=115200)
 lock = threading.RLock()
 
 
-def get_uid2():
-	cpt = 0
-	liste = []
-	while cpt<10:
-		uid = serialArduino.readline().decode("UTF-8")[:-2]
-		print(uid)
-		liste.append(uid)
-		cpt+=1
-	mc = Counter(liste).most_common()
-	serialArduino.flushOutput()
-	if len(mc[0][0])==0:
-		return None
-	else:
-		return mc[0][0]
-
-
-
 def get_uid():
 	serialArduino.flushOutput()
 	uid = ""
